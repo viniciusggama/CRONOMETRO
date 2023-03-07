1498d2b7a217aa7e9d24d590038020b2900323c8
Vinicius Gama Santos <114769520+viniciusggama@users.noreply.github.com>
2023-03-07 20:39:22 -0300

    atualização segundos

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 
+var sec = 00
 
+function start() {
+   setInterval(contar,1000)
+}
+
+function pause() {
+    console.log('PAUSOU')
+}
+
+function stop() {
+    console.log('PAROU')
+}
+
+//fazer a contagem a cada mile_segundos
+function contar() {
+     
+    sec++
+    var segundos = window.document.getElementById('segundos')
+    segundos.innerText = sec
+}
